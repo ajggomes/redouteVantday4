4b853198d8974a20edb2431d44ec015e0f9f036f
ip990196 <ip990196@RDPT-PC1JG31A.siege.red>
2017-10-26 09:44:07 +0100

    Com a classe validar Login

---
+++ src/main/java/com/redoutevant/_gitproj/Validarlogin.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.redoutevant._gitproj;
+
+/**
+ *
+ * @author Antonio gomes
+ * @serial 20171026
+ */
+public class Validarlogin {
+    String username;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    
+}
